3b3085cda02f84b252c5d56b744fa6957c9804bf
38say <owner@localhost>
2024-12-04 00:01:42 +0900

    できた

---
+++ src/main/java/com/techachademy/CountryRepository.java
@@ -0,0 +1,8 @@
+package com.techachademy;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, String> {
+
+}
---
+++ src/test/java/com/techachademy/TutorialJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techachademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
